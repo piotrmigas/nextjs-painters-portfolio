30a43169e872cc4f3a61c3835109750d96a1c7eb
piotrmigas <piotr.migas@icloud.com>
2021-09-18 13:03:49 +0200

    commit

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,7 +1,6 @@
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 import { motion } from "framer-motion";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import { worksImgs } from "../data/worksImgs";
 import { exhibitionImgs } from "../data/exhibitionImgs";
 
@@ -11,38 +10,32 @@ const Gallery = ({ options, loaded, onLoad }) => {
   return (
     <SimpleReactLightbox>
       <SRLWrapper options={options}>
-        <div className={`${!loaded ? "hidden" : "block"}`}>
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            variants={{
-              hidden: {
-                opacity: 0,
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              opacity: 0,
+            },
+            visible: {
+              opacity: 1,
+              transition: {
+                delay: 0.4,
               },
-              visible: {
-                opacity: 1,
-                transition: {
-                  delay: 0.4,
-                },
-              },
-            }}
-            className="grid grid-cols-4 sm:grid-cols-7 md:grid-cols-9 lg:grid-cols-12 gap-2 mx-auto max-w-6xl px-12 pb-12"
-          >
-            {(router.pathname === "/works" ? worksImgs : exhibitionImgs)
-              .sort((a, b) => b.id - a.id)
-              .map(({ id, src, caption }) => (
-                <div className="aspect-w-1 aspect-h-1 relative" key={id}>
-                  <Image
-                    src={src}
-                    alt={caption}
-                    onLoad={onLoad}
-                    className="object-cover cursor-pointer"
-                    layout="fill"
-                  />
-                </div>
-              ))}
-          </motion.div>
-        </div>
+            },
+          }}
+          className={`${
+            !loaded ? "hidden" : "block"
+          } grid grid-cols-4 sm:grid-cols-7 md:grid-cols-9 lg:grid-cols-12 gap-2 mx-auto max-w-6xl px-12 pb-12`}
+        >
+          {(router.pathname === "/works" ? worksImgs : exhibitionImgs)
+            .sort((a, b) => b.id - a.id)
+            .map(({ id, src, caption }) => (
+              <div className="aspect-w-1 aspect-h-1 relative" key={id}>
+                <img src={src} alt={caption} onLoad={onLoad} className="object-cover cursor-pointer" />
+              </div>
+            ))}
+        </motion.div>
       </SRLWrapper>
     </SimpleReactLightbox>
   );
