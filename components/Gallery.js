fc7b8d0d40165167d3426af37cd5db648ace22c7
piotrmigas <piotr.migas@icloud.com>
2021-09-20 17:55:39 +0200

    commit

--- components/Gallery.js
+++ components/Gallery.js
@@ -26,36 +26,36 @@ const Gallery = ({ options, setImgsLoaded, imgsLoaded }) => {
   }, []);
 
   return (
-    <SimpleReactLightbox>
-      <SRLWrapper options={options}>
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: {
-              opacity: 0,
-            },
-            visible: {
-              opacity: 1,
-              transition: {
-                delay: 0.4,
+    imgsLoaded && (
+      <SimpleReactLightbox>
+        <SRLWrapper options={options}>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                opacity: 0,
               },
-            },
-          }}
-          className={`${
-            !imgsLoaded ? "hidden" : "block"
-          } grid grid-cols-4 sm:grid-cols-7 md:grid-cols-9 lg:grid-cols-12 gap-2 mx-auto max-w-6xl px-12 pb-12`}
-        >
-          {images
-            .sort((a, b) => b.id - a.id)
-            .map(({ id, src, caption }) => (
-              <div className="aspect-w-1 aspect-h-1 relative" key={id}>
-                <img src={src} alt={caption} className="object-cover cursor-pointer" />
-              </div>
-            ))}
-        </motion.div>
-      </SRLWrapper>
-    </SimpleReactLightbox>
+              visible: {
+                opacity: 1,
+                transition: {
+                  delay: 0.4,
+                },
+              },
+            }}
+            className="grid grid-cols-4 sm:grid-cols-7 md:grid-cols-9 lg:grid-cols-12 gap-2 mx-auto max-w-6xl px-12 pb-12"
+          >
+            {images
+              .sort((a, b) => b.id - a.id)
+              .map(({ id, src, caption }) => (
+                <div className="aspect-w-1 aspect-h-1 relative" key={id}>
+                  <img src={src} alt={caption} className="object-cover cursor-pointer" />
+                </div>
+              ))}
+          </motion.div>
+        </SRLWrapper>
+      </SimpleReactLightbox>
+    )
   );
 };
 
