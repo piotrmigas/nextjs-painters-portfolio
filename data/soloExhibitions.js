0ea8c1ff7c0d432804f06b8428b3e5e04910e323
piotrmigas <piotr.migas@icloud.com>
2022-05-21 20:26:57 +0200

    commit

--- data/soloExhibitions.js
+++ data/soloExhibitions.js
@@ -1,4 +1,6 @@
 export const soloExhibitions = [
-  { id: 1, name: "2016 : Peekaboo : Henryk Gallery : Kraków : PL" },
-  { id: 2, name: "2020 : Screen : Raster Gallery : Warszawa : PL" },
+  { id: 1, name: '2016 : Peekaboo : Henryk Gallery : Kraków : PL' },
+  { id: 2, name: '2020 : Screen : Raster Gallery : Warszawa : PL' },
+  { id: 3, name: '2021/2022 : Prześwit : Gallery Grey House : Kraków : PL' },
+  { id: 4, name: '2022 : The Eyelid : East Contemporary Gallery : Mediolan : IT' },
 ];
