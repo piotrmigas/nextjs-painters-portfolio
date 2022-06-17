8ffaa815478dad0cded2475120351890f3a08482
piotrmigas <piotr.migas@icloud.com>
2022-06-17 22:55:29 +0200

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
                 </div>
               </div>
               <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-                <a href='https://www.instagram.com/emilia_kina' aria-label='Instagram' target='_blank' rel='noopener'>
+                <a href='https://www.instagram.com/emilia_kina' aria-label='Instagram' target='_blank' rel='noreferrer'>
                   <i className='fab fa-instagram' />
                 </a>
               </div>
